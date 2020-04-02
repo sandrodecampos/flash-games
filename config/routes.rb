4641da4848b6a98229b50ce4f2fba9455d4f6453
Sandro Campos <scampos80@live.com>
2020-04-02 11:37:53 -0500

    Added the root route, index and show for Products.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: [:index, :show]
+  # get '/products', to: 'products#index'
+  # get '/products/:id', to: 'products#show', id /\d/
+
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'products#index'
 end
