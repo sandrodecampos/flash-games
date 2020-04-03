65c51a4cfbeec8fb9af8fffacaa9169ccfe69e18
Sandro Campos <scampos80@live.com>
2020-04-03 00:33:36 -0500

    Added permit params for Product and columns/forms.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,42 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :developer, :publisher, :release_date, :platform_id, :genre_id
-end
+  permit_params :name, :description, :developer, :publisher, :release_date, :platform_id, :image,
+                category_items_attributes: [:id, :genre_id, :product_id]
+
+  index do
+    selectable_column # Checkbox
+    column :id
+    column :image do |product| # include thumb image
+      image_tag(product.image,width:100,height:50)
+    end
+    column :name
+    # column :description
+    # column :developer
+    # column :publisher
+    # column :release_date
+    # column :platform_id
+    column :genres do |product|
+      product.genres.map { |p| p.name }.join(", ").html_safe
+    end
+    column :created_at
+    column :updated_at
+    actions # view, edit, delete
+ end
+
+  form do |f|
+    f.semantic_errors
+        f.inputs "Add/Edit Post" do
+          f.input :name
+          f.input :description
+          f.input :developer
+          f.input :publisher
+          f.input :release_date
+          # f.input :genres, as: :check_boxes
+          f.has_many :category_items, allow_destroy: true do |n_f|
+            n_f.input :genre
+          end
+          f.input :image
+        end
+        f.actions
+
+    end
+  end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :platforms, through: :type_items
   has_many :category_items
   has_many :genres, through: :category_items
+  accepts_nested_attributes_for :category_items, allow_destroy: true
   has_one_attached :image
 
   paginates_per 6
