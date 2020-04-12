2bc7e8ea5be23119ddfa560ceef19ac3db63ec85
Sandro Campos <scampos80@live.com>
2020-04-11 22:50:10 -0500

    Generated Page Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/show'
   resources :products, only: [:index, :show]
   # get '/products', to: 'products#index'
   # get '/products/:id', to: 'products#show', id /\d/
